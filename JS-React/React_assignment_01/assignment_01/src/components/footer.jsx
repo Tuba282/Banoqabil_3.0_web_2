2fc3a13bb96661f9a6cd9fbe760ced7d7aafaf7d
Tuba282 <tubajan282@gmail.com>
2024-07-06 23:34:57 +0500

    assignment_01

---
+++ JS-React/React_assignment_01/assignment_01/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+
+function Footer() {
+    return (
+        <>
+            <div className="footer">
+                <p>Copyright © 2022. All rights reserved.</p>
+            </div>
+
+        </>
+    )
+}
+export default Footer;
--- JS-React/React_assignment_01/assignment_01/src/main.jsx
+++ JS-React/React_assignment_01/assignment_01/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './App.css'
+import Footer from "./components/footer"
+import Header from "./components/header"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Header />
     <App />
+    <Footer />
   </React.StrictMode>,
 )
